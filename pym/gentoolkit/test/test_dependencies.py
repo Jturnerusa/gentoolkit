232289c55caac55a9d04346f5f0f9f3b11127045
John Turner <jturner.usa@gmail.com>
2024-03-08 15:43:01 -0500

    test_dependencies.py: test for eval'ing USE in graph_reverse_depends

--- pym/gentoolkit/test/test_dependencies.py
+++ pym/gentoolkit/test/test_dependencies.py
@@ -69,3 +69,62 @@ def test_basic_revdeps(monkeypatch: MonkeyPatch) -> None:
         "app-misc/b-1.0",
         "app-misc/c-1.0",
     ]
+
+
+def test_evaluated_flags(monkeypatch: MonkeyPatch) -> None:
+    fake_depends = {
+        "app-misc/root-1.0": None,
+        "app-misc/e-1.0": {"DEPEND": "app-misc/root"},
+        "app-misc/f-1.0": {"DEPEND": "foo? ( app-misc/e )"},
+        "app-misc/g-1.0": {"DEPEND": "bar? ( app-misc/f )"},
+        "app-misc/h-1.0": {"DEPEND": "foo? ( bar? ( app-misc/e ) )"},
+        "app-misc/i-1.0": {"DEPEND": "never? ( app-misc/root )"},
+    }
+    fake_pkgs = list(fake_depends.keys())
+    # we will mutate the list to change the monkey patched use flags
+    use: List[str] = []
+
+    def e(self, env_vars):
+        return environment(self, env_vars, fake_depends, fake_pkgs)
+
+    def g(self):
+        return use
+
+    monkeypatch.setattr(Dependencies, "environment", e)
+    monkeypatch.setattr(Dependencies, "_get_active_use", g)
+
+    assert sorted(
+        pkg.cpv
+        for pkg in Dependencies("app-misc/root").graph_reverse_depends(
+            pkgset=fake_pkgs, only_direct=False, evaluate_use=True
+        )
+    ) == [
+        "app-misc/e-1.0",
+    ]
+
+    use = ["foo"]
+    assert sorted(
+        pkg.cpv
+        for pkg in Dependencies("app-misc/root").graph_reverse_depends(
+            pkgset=fake_pkgs, only_direct=False, evaluate_use=True
+        )
+    ) == ["app-misc/e-1.0", "app-misc/f-1.0"]
+
+    # this test looks like it's incorrect, but the algorithm for evaluating use flags only
+    # takes into account the conditional that is directly adjacent to the dep in the DEPEND
+    # expression.
+    use = ["bar"]
+    assert sorted(
+        pkg.cpv
+        for pkg in Dependencies("app-misc/root").graph_reverse_depends(
+            pkgset=fake_pkgs, only_direct=False, evaluate_use=True
+        )
+    ) == ["app-misc/e-1.0", "app-misc/h-1.0"]
+
+    use = ["foo", "bar"]
+    assert sorted(
+        pkg.cpv
+        for pkg in Dependencies("app-misc/root").graph_reverse_depends(
+            pkgset=fake_pkgs, only_direct=False, evaluate_use=True
+        )
+    ) == ["app-misc/e-1.0", "app-misc/f-1.0", "app-misc/g-1.0", "app-misc/h-1.0"]
